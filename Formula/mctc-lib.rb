3fa10f3bded66914532d78ca17329674b6745f52
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-02-11 12:02:07 +0000

    mctc-lib: add 0.2.4 bottle.

--- Formula/mctc-lib.rb
+++ Formula/mctc-lib.rb
@@ -5,6 +5,13 @@ class MctcLib < Formula
   sha256 "ca728d129c4bb0a06ed37183e3a8e2659d5fbbec2278c3f6569fb638314129d1"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/awvwgk/homebrew-qc/releases/download/mctc-lib-0.2.4"
+    sha256 cellar: :any,                 big_sur:      "7831fa1494e7cbf3bc3cc6f8029d9128e05ff32f256ee11ea8829ffe20e62d0c"
+    sha256 cellar: :any,                 catalina:     "0bba6fbe2d29266addd4c6b74365dc0c9a2cd13774ed3d791b71f544bae24cfe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4f04e14f85d280328eb60014064c00f749b7a85ede8faf2632d64e3922f5f02c"
+  end
+
   depends_on "asciidoctor" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
