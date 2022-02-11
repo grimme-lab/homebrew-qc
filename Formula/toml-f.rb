9403c00687700d620a612188e69ee94ccb2dc837
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-02-11 13:07:09 +0000

    toml-f: add 0.2.2 bottle.

--- Formula/toml-f.rb
+++ Formula/toml-f.rb
@@ -5,6 +5,13 @@ class TomlF < Formula
   sha256 "c3e5e1f57de00977b5e41d3a857ddb7e6b41f0b9116ed608cf89f295d9ef24c6"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    root_url "https://github.com/awvwgk/homebrew-qc/releases/download/toml-f-0.2.2"
+    sha256 cellar: :any,                 big_sur:      "b8a0e56bcc31d153880d1c16732f9a670d27abd2e976086f0ffb39f941ff4341"
+    sha256 cellar: :any,                 catalina:     "81b128c1b3d81af0a9568ab40b4f352f379ae04df02597fa1e9851a93e2e666d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a801393ee4a9e6f45bf80f93dab497527114c88e89d3943183b29cee35955df3"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :test
