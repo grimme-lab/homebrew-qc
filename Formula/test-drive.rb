70cf9dd80773fc11cc259bc13ed8b2344e9552bf
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-05-17 11:49:54 +0000

    test-drive: add 0.4.0 bottle.

--- Formula/test-drive.rb
+++ Formula/test-drive.rb
@@ -5,6 +5,13 @@ class TestDrive < Formula
   sha256 "f9c037a3c1727e98801c2375e6f2efde9881ac1f54b04be3bc928e094f5787a5"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    root_url "https://github.com/grimme-lab/homebrew-qc/releases/download/test-drive-0.4.0"
+    sha256 cellar: :any,                 big_sur:      "4a3bc8175b1107fdfd8b6e77effb413333e853e69d5d1f98e258b69aa9d73773"
+    sha256 cellar: :any,                 catalina:     "cbc920050e2a7f7acc2f8532349b31d1d9f76a58bad5fe5476f4ae679146ba4f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "95ce8120d56b56ebb386207883fbb59f717d212c19fd4770a88e2650cb808e46"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :test
