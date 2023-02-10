f8528564202c0cd62b417cb639815dd23b163930
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2023-02-10 10:46:42 +0100

    toml-f 0.3.1

--- Formula/toml-f.rb
+++ Formula/toml-f.rb
@@ -1,8 +1,8 @@
 class TomlF < Formula
   desc "TOML parser implementation for data serialization and deserialization in Fortran"
-  homepage "https://toml-f.github.io/toml-f"
-  url "https://github.com/toml-f/toml-f/archive/v0.2.2.tar.gz"
-  sha256 "c3e5e1f57de00977b5e41d3a857ddb7e6b41f0b9116ed608cf89f295d9ef24c6"
+  homepage "https://toml-f.readthedocs.io"
+  url "https://github.com/toml-f/toml-f/releases/download/v0.3.1/toml-f-0.3.1.tar.xz"
+  sha256 "7f40f60c8d9ffbb1b99fb051a3e6682c7dd04d7479aa1cf770eff8174b02544f"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
@@ -14,7 +14,8 @@ class TomlF < Formula
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "pkg-config" => :test
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "test-drive" => :build
   depends_on "gcc"
   fails_with gcc: "4"
   fails_with gcc: "5"
@@ -24,12 +25,10 @@ class TomlF < Formula
 
   def install
     ENV.fortran
-    inreplace "config/install-mod.py", /python$/, "python3"
     meson_args = std_meson_args
     system "meson", "setup", "_build", *meson_args
     system "meson", "compile", "-C", "_build"
-    selected_tests = ["version", "fpm", "tftest", "example-1.1", "example-2.2", "decoder"]
-    system "meson", "test", "-C", "_build", "--no-rebuild", "--num-processes", "1", *selected_tests
+    system "meson", "test", "-C", "_build", "--no-rebuild", "--num-processes", "1"
     system "meson", "install", "-C", "_build", "--no-rebuild"
   end
 
