894ac56bb638fccb676d3134b9ca644266ea0c3f
Marcel Müller <marcel.mueller@thch.uni-bonn.de>
2024-11-20 14:54:54 +0100

    remove inreplace command
    Signed-off-by: Marcel Müller <marcel.mueller@thch.uni-bonn.de>

--- Formula/mstore.rb
+++ Formula/mstore.rb
@@ -25,7 +25,6 @@ class Mstore < Formula
 
   def install
     ENV.fortran
-    inreplace "config/install-mod.py", /python$/, "python3"
     meson_args = std_meson_args
     system "meson", "setup", "_build", *meson_args
     system "meson", "compile", "-C", "_build"
